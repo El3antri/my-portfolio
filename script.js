4b650b379f4447cb66e6398c2cefcd64ea9a9ab2
Ahmed Antri <ahmedantri1999@gmail.com>
2024-12-01 13:01:43 +0100

    Portfolio

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Welcome to my portfolio!");
+// Carousel scrolling
+const carousel = document.querySelector('.carousel');
+carousel.addEventListener('wheel', (e) => {
+    e.preventDefault();
+    carousel.scrollBy({
+        left: e.deltaY < 0 ? -200 : 200,
+    });
+});
